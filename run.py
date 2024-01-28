afaf3bffa76ab29b6fa5c7abd121b42ae04c156c
Megan Walford <mwalford22@gmail.com>
2024-01-28 14:06:12 +0000

    Created templates .html pages

--- run.py
+++ run.py
@@ -5,7 +5,15 @@ app = Flask(__name__)       #single built in python variable
 
 @app.route("/")             #route decorator, also called a pie notion, a decorator is a way of wrapping funtions
 def index():
-    return render_template("index.html")
+    return render_template("index.html")            #seeks the index.html
+
+@app.route("/about")            
+def about():
+    return render_template("about.html")            #seeks the about.html
+
+@app.route("/contact")            
+def contact():
+    return render_template("contact.html")            #seeks the about.html
 
 if __name__ == "__main__":                          #__main__ is the default module in python, is the first module to be run so if its not imported 
     app.run(
